2df11bf12ac6c11091de746772b0617a5cb5dbd1
KHiltenb <kenhiltenbrand@gmail.com>
2021-04-16 23:25:36 -0500

    Need to branch to start a backup Plan

---
+++ Logs.php
@@ -0,0 +1,30 @@
+<?php
+/*
+LOGS FILE - 
+
+04/16/2021 - Thought of this super late in the semester to track my work on this project and express
+thoughts/feelings about the project as time goes on. I have under a month to finish this, a 12 page
+research paper, and somehow fit studying for my diff. eq. and linear algebra classes in as well. I'm
+pretty confident that I'll pull it off while slowly losing sanity and falling into a coma from caffeine
+use, but the grades will be well-worth the effort if I get all A's. 
+Now onto the project, I still don't have the data from the dean, which is frustrating to say the
+least, given that it's the whole crux of my project. We need to turn in the MySQL scripts that create
+the database and since I have no data, no database can be created. However, I do possess a test database
+that can be used in the case that the WCC dean doesn't come through with the goods. It's not very
+comprehensive, and would make the project more of a proof of concept than anything which sucks, but
+if it's the best that I can do with what I have, then I hope professor Daly understands. If I 
+don't get the data within the week, I'll email professor Daly requesting that my database contents
+be admitted as is until the data comes from the dean, and I'll ask the dean myself, which I probably
+should have done a while ago. Problem is, I don't know which dean to contanct. I know Mr. Little,
+and have some history with him, so maybe if I explain that this is for a class and would be willing
+to work with WaWa's TAC to make it into a viable tool for the MyWCC portal, maybe he'd be inclined
+to help me out. Ultimately, if the data is never recieved, I'm going to have to have a recourse.
+An idea that just dawned on me is to still have separate arrays for the different classes and whatnot
+but to have the user enter them if all goes to hell and I have no data. I'll just have to modify my 
+database by a little bit and batta bing batta boom we got ourselves a recourse. I'll start developing 
+that on the side to ensure that I have a functional project to present. This will be the end of my log
+for the night. 
+
+mm/dd/yyyy - 
+*/
+?>
