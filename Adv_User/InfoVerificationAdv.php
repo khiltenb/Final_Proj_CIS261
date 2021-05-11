fee6a27690f67c1351db514560d07f0c4af7c928
KHiltenb <kenhiltenbrand@gmail.com>
2021-05-10 19:37:12 -0500

    Before project presentation tomorrow

--- Adv_User/InfoVerificationAdv.php
+++ Adv_User/InfoVerificationAdv.php
@@ -3,151 +3,59 @@
     CIS261 Professor Daly
     Final Project - InfoVerificationAdv.html
 -->
-<!DOCTYPE html>
-<!--php header inclusion -->
-<html>
 <?php $path ='../'?>
-<?php include '../view/header.php'?>
-
-<?php 
-//Need logic for table here
-
-//Pseudo code here
-/*
-get all data from classInfoTemp as an array
-for each (for loop maybe) day
-    count the number of events on that day
-    check for conflicts
-    if conflicts exist, check what overlaps what and increase the colspan number for that day and the column number for the week
-    this is gonna be pretty hard.
-*/
-
-function isOverlap($class1, &$class2) {
-    if ($class1['sTime'] < $class2['sTime']) {      //
-        if ($class2['sTime'] < $class1['eTime']) {
-            return true;
-        }
-    } else if ($class1['sTime'] == $class2['sTime']) {
-        return true;
-    } else {
-        // class one starts before class two
-        if ($class2['eTime'] > $class1['sTime']) {
-            return true;
-        }
-    }
-
-}
-
-function insertClassIntoDay($class, &$day) {
-    for ($col = 0; $col < count($day); $col++){// for each column of the day
-        $overlap = false;
-        for ($eventIndex = 0; $eventIndex < count($day[$col]); $eventIndex++){// scan the list of events for the column for an overlapping class time
-            $colEvent = $day[$col][$eventIndex];
-            if (isOverlap($class, $colEvent)) {
-                $overlap = true;
-                break; // This column won't do, go to the next one.
-            } else if ($colEvent['sTime'] > $class['sTime']) {// if there is no overlap, and this event goes before index, insert.
-                $day[$col] = array_splice($day[$col], $eventIndex, 0, $class);
-                return;
-            }
-        }
-        if (!$overlap) {
-            // There was no overlap in this column.  We just hit the end, so insert at the end.
-            $day[$col][] = $class;
-                    return;
-        }
-    }
-    //if there were overlaps in all of the columns, insert a new column
-    $day[] = array($class);
-}
-
-
-
-// My data for the rendering.
-
-$data = array();
-for ($i = 0; $i < 7;$i++) {
-    $data[] = array();          //  Array level 1 for the days of the week
-    $data[$i][] = array();      //  Array level 2 for the column of the day always having at least one
-}
-
-$classData = getTemp();                 //  Set arrays to catch the data for the days
-$days = array('CMON', 'CTUE', 'CWED', 'CTHU', 'CFRI', 'CSAT', 'CSUN');  //  Associative array to help with parsing through the data
-for ($c = 0;$c < count($classData); $c++) {                             //  for each entry that was retrieved from the classInfoTemp Table, iterate the following
-    for ($d = 0; $d < count($days); $d++) {                             //   for each day of the week, do the following
-        if ($classData[$c][$days[$d]] != 'None') {                      //  if there is no class meeting time on this day, do the following
-            $classTimeTemp = explode('-', $classData[$c][$days[$d]]);   //  separate the start and end time, and set the classTimeTemp variable to the given array
-            $thisClass = array('sTime'=>(int)$classTimeTemp[0], 'eTime'=>(int)$classTimeTemp[1], 'desc'=>$classData[$c]['CourseID']);
-            insertClassIntoDay($thisClass, $data[$d]);
-        }
-    }
-}
-
-?>
+<?php include '../view/header.php'; ?>
 
 <body>
-    <h1>Proto-Schedule</h1>
-    <table cellspacing=0>
-      <tbody>
+    <h1>Please Verify the Information Presented</h1>
+    <p>The SQL should run here showing the added details about the class to let the
+        student confirm the data shown. I've added a "delete" button, but it doesn't
+        work as intended just yet (issues with scope). This is just proof that the SQL for the class time and
+        office hours works, and the proof for the schedule personalization will work
+        much the same as this.
+     </p>
+    <table>
         <tr>
-            <th class="unique"></th>
-            <th colspan="<?php echo count($data[0]); ?>">Monday</th>
-            <th colspan="<?php echo count($data[1]); ?>">Tuesday</th>
-            <th colspan="<?php echo count($data[2]); ?>">Wednesday</th>
-            <th colspan="<?php echo count($data[3]); ?>">Thursday</th>
-            <th colspan="<?php echo count($data[4]); ?>">Friday</th>
-            <th colspan="<?php echo count($data[5]); ?>">Saturday</th>
-            <th colspan="<?php echo count($data[6]); ?>">Sunday</th>
+            <th>CRN</th>
+            <th>Course ID</th>
+            <th>Prof.</th>
+            <th>Mon</th>
+            <th>Tues</th>
+            <th>Weds</th>
+            <th>Thur</th>
+            <th>Fri</th>
+            <th>Sat</th>
+            <th>Sun</th>
         </tr>
-        <?php for($o = 0; $o < 288; $o++): ?>
-            <tr>
-            <?php if(($o)%12 == 0):?>
-                <td class="whole_hour"><?php echo ($o)/12 ?>&nbsp;am</td>
-            <?php else: ?>
-                <td class=<?php if($o%3 == 0){ echo 'major_hour';} else {echo 'minor_hour';} ?>>:<?php echo ((($o)%12) * 5)?> </td>
-            <?php endif;?>
-            <?php 
-            for($dow = 0; $dow < count($data); $dow++){
-                for($cow = 0; $cow < count($data[$dow]); $cow++){
-                    $foundEvent = false;
-                
-                    $timeNow = (intdiv($o, 12) * 100) + (($o%12)*5);
-                    for($e = 0; $e < count($data[$dow][$cow]); $e++) {
-                        // scan the event for this day to find an entry for this time slot
-                        $event = $data[$dow][$cow][$e];
-                        $event['sTime'] = $event['sTime'] - ($event['sTime']%5);        // Rounding to nearest five minutes
-                        $event['eTime'] = $event['eTime'] - ($event['eTime']%5);        // Rounding to nearest five minutes
-                        if ($timeNow == $event['sTime']){
-                            $foundEvent = true;
-                            $startTimeH = intdiv($event['sTime'], 100);
-                            $startTimeM = ($event['sTime']%100) / 5;
-                            $endTimeH = intdiv($event['eTime'], 100);
-                            $endTimeM = ($event['eTime']%100) / 5;
-                            $durationSpan = (($endTimeH - $startTimeH) * 12) + ($endTimeM - $startTimeM);
-                            echo '<td rowspan="'. $durationSpan .'">'. $event['desc'] .'</td>';
-                            break;  // This event occurs at this row
-                        } else if (($timeNow > $event['sTime']) && ($timeNow < $event['eTime'])){
-                            $foundEvent = true;
-                            break; // this event falls after this row
-                        } else {
-                        }
-                        // if the event starts now, output the event
-                        // else if the event spans over now but doesn't start now don't output a td
-                    }
-                    if (!$foundEvent) {
-                        echo '<td></td>';
-                    }
-                }
-            } ?>
-            </tr>
-        <?php endfor; ?>
+
+        <?php $class = array(); ?>
+        <?php //for($i = 0; $i <= $_SESSION['count']; $i++): ?>
+        <?php //$class = $classinformation[$i] ?>
+        <?php foreach($classinformation as $class):?>
+        <tr>
+            <td><?php echo $class['CRN']; ?></td>
+            <td><?php echo $class['CourseID']; ?></td>
+            <td><?php echo $class['Professor']; ?></td>
+            <td><?php echo $class['CMON']; ?></td>
+            <td><?php echo $class['CTUE']; ?></td>
+            <td><?php echo $class['CWED']; ?></td>
+            <td><?php echo $class['CTHU']; ?></td>
+            <td><?php echo $class['CFRI']; ?></td>
+            <td><?php echo $class['CSAT']; ?></td>
+            <td><?php echo $class['CSUN']; ?></td>
+            <td><form action='.' method='POST'>
+                <input type='hidden' name='action' value='Remove Class'>
+                <input type='hidden' name='recordNum' value='<?php echo $class['CRN'] ?>'>
+                <input type='submit' value='Delete'>
+            </form></td>
+        </tr>
+        <?php endforeach;?>
+    </table>
+
     <form action="." method="POST" style="display:inline">
-        <input type="submit" name='action' value="Download">
+        <input type="submit" name='action' value="Enter Other CRNs">
     </form>
     <form action="." method="POST" style="display:inline">
-        <input type="submit" name='action' value="Exit">
+        <input type="submit" name='action' value="Continue to the Prototype Schedule">
     </form>
-    <form action="." method="POST" style="display:inline">
-        <input type="submit" name='action' value="Continue to Modified Schedule">
-    </form>
-<?php include '../view/footer.php';?>
+<?php include '../view/footer.php'; ?>
